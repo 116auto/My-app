b87f9ab7fd3bdc3f00616f0c0893f2b22a8af33f
Aleks <autoaleks@me.com>
2019-08-21 09:20:39 +0300

    new commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className='header'>
+    <img src='https://s1.logaster.com/static/v3/img/first_step_seo/example-1.png' />
+  </header>
+}
+
+export default Header;
